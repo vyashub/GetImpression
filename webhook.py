4facf4e7088fc2b4f3e0bb0bc9f2279e826f4d8c
Shubhendra Vyas <shubhendravyas51@gmail.com>
2018-10-31 18:30:22 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
-    print(json.dumps(req, indent=4))
+    #print(json.dumps(req, indent=4))
     
     res = makeResponse(req)
     
